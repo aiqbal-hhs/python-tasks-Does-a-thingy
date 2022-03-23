9befff5601ab0c3e4d9e5d7604525317f5daa51b
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2022-03-24 10:11:57 +1300

    Update something_new.py

--- something_new.py
+++ something_new.py
@@ -3,14 +3,7 @@ frank = turtle.Turtle()
 frank.speed(0)
 frank.color('blue')
 
-for i in range(100):
-    frank.fd(184)
-    frank.lt(27)
-    frank.fd(10)
-    frank.lt(74)
-
-    
-for i in range(120):
+for i in range(200):
     #Change colors
     if i == 20:
         frank.color('red')
@@ -23,7 +16,7 @@ for i in range(120):
     elif i == 100:
         frank.color('pink')
     #Draw the spirograph
-    frank.forward(200)
-    frank.left(184)
-    frank.forward(40)
-    frank.right(30)
+    frank.fd(184)
+    frank.lt(27)
+    frank.fd(10)
+    frank.lt(74)
